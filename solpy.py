5624182f978408000a3d59cfa54ea8435ea4e434
Denis Volk <denis.volk@gmail.com>
2014-12-04 13:16:32 +0100

    etc

--- solpy.py
+++ solpy.py
@@ -1,13 +1,14 @@
 import math
+import time
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 class body:
-    def __init__(self, p, v, m):
+    def __init__(self, p, v, m, r):
         self.position, self.velocity, self.mass = np.array(p), np.array(v), m
-        self.trace_x, self.trace_y, self.acceleration = [], [], np.zeros(3)
-        self.new_acceleration = np.zeros(3)
+        self.acceleration, self.radius, self.trace = np.zeros(3), r, None
+        self.new_acceleration = None
 
 def influence(b1, bodies, G):
     A = 0
@@ -18,10 +19,11 @@ def influence(b1, bodies, G):
     return G * A
 
 def simulate_leapfrog(bodies, steps, dt, G):
+    for b in bodies:
+        b.trace = np.zeros(shape=(steps, 2))
     for i in xrange(steps):
         for b in bodies:
-            b.trace_x.append(b.position[0])
-            b.trace_y.append(b.position[1])
+            b.trace[i] = b.position[:2]
         for b in bodies:
             b.acceleration = influence(b, bodies, G)
         for b in bodies:
@@ -33,49 +35,87 @@ def simulate_leapfrog(bodies, steps, dt, G):
         for b in bodies:
             b.acceleration = b.new_acceleration
 
+def center_offset(bodies):
+    """Calculates X and V offset for central body"""
+    C = np.zeros(3)
+    V = np.zeros(3)
+    M = 0
+    for b in bodies:
+        M += b.mass
+        C += b.mass * b.position
+        V += b.mass * b.velocity
+    return - C / M, - V / M
+
 def draw_traces(bodies):
     for b in bodies:
-        plt.plot(b.trace_x, b.trace_y)
+        circle = plt.Circle((b.trace[0, 0], b.trace[0, 1]), radius=b.radius, fc='y')
+        plt.gca().add_patch(circle)
+    for b in bodies:
+        plt.plot(b.trace[:,0], b.trace[:,1])
     plt.axis('scaled')
+    #plt.axis('off')
+    #frame1 = plt.gca()
+    #frame1.axes.get_xaxis().set_visible(False)
+    #frame1.axes.get_yaxis().set_visible(False)
+    plt.grid()
+    plt.savefig("/home/dv/ipy/output-%s.png" % int(time.time()), dpi=300)
     plt.show()
 
-def toy():
-    b1 = body([0.0, 0, 0], [0.0, 0.0, 0], 10)
-    b2 = body([-1.0, 1.0, 0], [1.7, 0, 0], 0.001)
-    b3 = body([1.0, -1.0, 0], [-1.7, 0, 0], 0.001)
-    return 0.01, 1, [b1, b2, b3]
+def random_bodies():
+    N = 10
+    X = np.random.uniform(size=(N,3))
+    V = np.random.uniform(size=(N,3))
+    M = np.random.uniform(size=N)
+    bodies = []
+    for x, v, m in zip(X, V, M):
+        bodies.append(body(x, v, m, 0.01))
+    return 0.001, 1, bodies
+
+def earth_and_moon():
+    ship_phase = math.pi*1.53
+    ship_orbit_height = 1e8
+    ship_vel = 2506.265
+    earth = body([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], 5.98e+24, 6.378100e+6)
+    moon = body([earth.position[0] + 3.84399e8, 0.0, 0.0], \
+                [0.0, 1022 + earth.velocity[1], 0.0], 7.3477e22, 0.173814e6)
+    ship = body([earth.position[0] + ship_orbit_height * math.cos(ship_phase), \
+                 earth.position[1] + ship_orbit_height * math.sin(ship_phase), 0.0], \
+                [earth.velocity[0] + ship_vel * math.cos(ship_phase + math.pi/2), \
+                 earth.velocity[1] + ship_vel * math.sin(ship_phase + math.pi/2), 0.0], 1e8, 1e4)
+
+    bodies = [earth, ship, moon]
+    C, V = center_offset(bodies)
+    earth.position += C
+    earth.velocity += V
+
+    return 0.024 * 3600, 6.67428e-11, bodies
 
 def solar_system():
     dt = 24 * 3600
     G = 6.67428e-11
 
-    ship_phase = math.pi*1.53
-    ship_orbit_height = 1e8
-    ship_vel = 2516.265
+    sun = body([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], 1.9891e+30, 696.000000e+6)
+    mercury = body([0.57909175e+11, 0.0, 0.0], [0.0, 47872.5, 0.0], 3.302e+23, 2.439640e+6)
+    venus = body([1.08208930e+11, 0.0, 0.0], [0.0, 35021.4, 0.0], 4.869e+24, 6.051590e+6)
+    earth = body([1.49597871e+11, 0.0, 0.0], [0.0, 29800.0, 0.0], 5.98e+24, 6.378100e+6)
+    mars = body([2.27936640e+11, 0.0, 0.0], [0.0, 24130.9, 0.0], 6.4191e+23, 3.397000e+6)
+    jupiter = body([0.0, 7.40573600e+11 , 0.0], [-13720, 0.0, 0.0], 1.8986e+27, 71.492680e+6)
+    saturn = body([-14.26725400e+11, 0.0, 0.0], [0.0, -9672.4, 0.0], 5.6851e+26, 60.267140e+6)
+    uranus = body([0.0, -28.70972200e+11, 0.0], [6835.2, 0.0, 0.0], 8.6849e+24, 25.557250e+6)
+    neptune = body([44.98252900e+11, 0.0, 0.0], [0.0, 5477.8, 0.0], 1.0244e+26, 24.766360e+6)
 
-    sun = body([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], 1.9891e+30)
-    mercury = body([0.57909175e+11, 0.0, 0.0], [0.0, 47872.5, 0.0], 3.302e+23)
-    venus = body([1.08208930e+11, 0.0, 0.0], [0.0, 35021.4, 0.0], 4.869e+24)
-    earth_sol = body([1.49597871e+11, 0.0, 0.0], [0.0, 29800.0, 0.0], 5.98e+24)
-    earth_alone = body([1.49597871e+11, 0.0, 0.0], [0.0, 0.0, 0.0], 5.98e+24)
-    earth = earth_sol
-    moon = body([earth.position[0] + 3.84399e8, 0.0, 0.0], \
-                [0.0, 1022 + earth.velocity[1], 0.0], 7.3477e22)
-    mars = body([2.27936640e+11, 0.0, 0.0], [0.0, 24130.9, 0.0], 6.4191e+23)
-    ship = body([earth.position[0] + ship_orbit_height * math.cos(ship_phase), \
-                 earth.position[1] + ship_orbit_height * math.sin(ship_phase), 0.0], \
-                [earth.velocity[0] + ship_vel * math.cos(ship_phase + math.pi/2), \
-                 earth.velocity[1] + ship_vel * math.sin(ship_phase + math.pi/2), 0.0], 1e6)
-    jupiter = body([7.78357721e+11, 0.0, 0.0], [0.0, 13100, 0.0], 1.9e+27)
-    saturn = body([14.26725400e+11, 0.0, 0.0], [0.0, 9672.4, 0.0], 5.6851e+26)
-    uranus = body([28.70972200e+11, 0.0, 0.0], [0.0, 6835.2, 0.0], 8.6849e+24)
-    neptune = body([44.98252900e+11, 0.0, 0.0], [0.0, 5477.8, 0.0], 1.0244e+26)
+    bodies = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+    C, V = center_offset(bodies)
+    sun.position += C
+    sun.velocity += V
 
-    return dt, G, [sun, mercury, venus, earth, moon, mars, jupiter, saturn, uranus, neptune]
+    return dt, G, bodies
 
 def main():
     dt, G, bodies = solar_system()
-    simulate_leapfrog(bodies, 300, dt, G)
+    print center_offset(bodies)
+    simulate_leapfrog(bodies, 2*365, dt, G)
+    print center_offset(bodies)
     draw_traces(bodies)
 
 main()
